06e5f70de444396fb94e79c4ba1e7fdbf4488a45
Jervis Bay <jervis.bay@gmail.com>
2020-05-30 12:16:45 -0400

    remove compression from worksoutcontroller

--- controllers/workouts_controller.js
+++ controllers/workouts_controller.js
@@ -5,8 +5,6 @@ var connection = require("../config/connection");
 var cTable = require('console.table');
 const db = require("../models");
 
-const compression = require("compression");
-app.use(compression());
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
